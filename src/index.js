aef0a3a3bd021d6a01b69dc7e734d24d75581307
trytestlearnworkadaptpray <troggsfan@protonmail.com>
2020-07-30 18:31:17 -0400

    step1 again

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res)=> {
+  res.json('Hello World!')
+})
+
 app.listen(3000, ()=> console.log('Server is running on 3000'))
